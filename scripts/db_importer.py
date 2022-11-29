f47ca963d8ac3b1110635d1d5b44871817cc069d
Brian Guayante <61845137+bguayante@users.noreply.github.com>
2022-11-29 10:56:44 -0800

    copied db_importer.py from `main`

--- scripts/db_importer.py
+++ scripts/db_importer.py
@@ -1,60 +1,33 @@
-"""
-Imports validated JSON-LD data into an RDF datastore.
-"""
-
 import os
-import sys
 import glob
 from dotenv import load_dotenv
 from rdflib.graph import Graph
-from rdflib.store import Store
-from rdflib import plugin
+import sqlite3
 
+# IMPORT GRAPHS INTO DATASTORE #
 
-def main():
-    """
-    Script to import validated JSON-LD data into an RDF store.
-    """
-    # load environmental variables
-    load_dotenv()
-    DB = os.getenv("DB_LOC") + "court-data.db"
+# load environmental variables
+load_dotenv()
+DB = os.getenv("DB_LOC") + "court-data.db"
 
-    ### dev ###
-    if os.path.exists(DB):
-        os.remove(DB)
-    ### dev ###
+### dev ###
+if os.path.exists(DB):
+    os.remove(DB)
+### dev ###    
 
-    # collect all JSON files in valid_json dir
-    # valid_json_files = glob.glob("data/valid_json/*.json")
-    valid_json_files = glob.glob("../data/raw_json/*.json")
+valid_json_files = glob.glob("data/valid_json/*.json")
+conn =f"sqlite:///{DB}"
 
-    # configure db connection string
-    # this does not have to be sqlite
-    # mySQL and postgres are also supported
-    conn = f"sqlite:///{DB}"
+graph = Graph("SQLAlchemy", identifier='court_data')
+graph.open(conn, create=True)
 
-    # configure dialect/engine
-    store = plugin.get("SQLAlchemy", Store)(identifier="court_data_store")
-    graph = Graph("SQLAlchemy", identifier='court_data_graph')
+for file in valid_json_files: 
+    graph.parse(file, format="json-ld")
 
-    # connect to db
-    graph.open(conn, create=True)
+result = graph.query("select * where {?s ?p ?o}")
 
-    # parse and import data from collected JSON files
-    for file in valid_json_files:
-        graph.parse(file, format="json-ld")
+# Testing, return all records
+for subject, predicate, object_ in result:
+    print(subject, predicate, object_)
 
-    # testing #
-    result = graph.query("select * where {?s ?p ?o}")
-
-    for subject, predicate, object_ in result:
-        print(subject, predicate, object_)
-    # /testing #
-
-    graph.close()
-
-    return 1
-
-
-if __name__ == "__main__":
-    main()
+graph.close()
