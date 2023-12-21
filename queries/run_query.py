4bb4f7d4cd26aeb016488f3427b01ed3999a939a
bguayante <bguayante@gmail.com>
2023-12-21 14:10:33 -0800

    added option to specify query export location

--- queries/run_query.py
+++ queries/run_query.py
@@ -3,6 +3,8 @@ Exports validated JSON-LD data from an RDF datastore.
 """
 
 import os
+import sys
+import argparse
 from dotenv import load_dotenv
 from rdflib.graph import Graph
 from rdflib.store import Store
@@ -14,23 +16,42 @@ def main():
     """
     Script to export validated JSON-LD data out of a RDF store.
     """
+
+    # Define location to which results json will be saved from arg. Defaults to ./results/ if no
+    # argument provided.
+    def parse_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--loc",
+            help='The location to which the query results are saved. Defaults to "./results/"',
+            default="queries/results/",
+            dest="results_path",
+            nargs="?",
+        )
+
+        args = parser.parse_args()
+        return args.results_path
+
     # load environmental variables
     load_dotenv()
     DB = os.getenv("DB_LOC") + "/court-data.db"
 
-    graph = Graph(store='Oxigraph', identifier='http://court')
+    graph = Graph(store="Oxigraph", identifier="http://court")
     # connect to db
     graph.open(DB)
 
     # load query
-    with open('./queries/all_records.sparql') as qf:
+    with open("./queries/all_records.sparql") as qf:
         qt = qf.read()
 
     qr = graph.query(qt)
 
-    to_isomorphic(qr).serialize(
-        'queries/results/query_result.json', format='json-ld')
-    print('Query complete. Check /queries/results/query_result.json for results.\n')
+    results_path = parse_args()
+    if not os.path.exists(results_path):
+        os.makedirs(results_path)
+
+    to_isomorphic(qr).serialize(f"{results_path}/query_result.json", format="json-ld")
+    print(f"Query complete. Check {results_path}/query_result.json for results.\n")
 
     graph.close()
 
